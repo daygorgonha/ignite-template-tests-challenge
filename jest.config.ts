adc7411ff03a1c75dc0b2aa6a61db224278036e2
Dayane B X Gorgonha <dayanebismark18@gmail.com>
2022-04-27 11:38:48 -0300

    add jest configuration

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import { pathsToModuleNameMapper } from "ts-jest/utils";
+import { compilerOptions } from "./tsconfig.json";
+
+export default {
+   bail: true,
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ["<rootDir>/src/modules/**/useCases/**/*.ts"],
+  coverageProvider: "v8",
+  coverageReporters: ["text-summary", "lcov"],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: "<rootDir>/src/",
+  }),
+  preset: "ts-jest",
+  testMatch: ["**/*.spec.ts"],
+}
